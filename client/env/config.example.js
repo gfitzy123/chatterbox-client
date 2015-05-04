2a4effc1257dfa0b12f42628bc059d63dbb1fd5e
Student <student@MKS-SF-4.local>
2015-05-04 15:44:38 -0500

    send tests passing

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -13,6 +13,14 @@ if (!/(&|\?)username=/.test(window.location.search)) {
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "PARSE_APP_ID");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "PARSE_API_KEY");
+  jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
+  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+// PARSE_APP_ID: voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r
+// PARSE_API_ID: QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf
+
+// curl -X GET \
+//   -H "X-Parse-Application-Id: voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r" \
+//   -H "X-Parse-REST-API-Key: QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf" \
+//   https://api.parse.com/1/classes/chatterbox/B0xl7amJFM
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,48 @@
 // YOUR CODE HERE:
 
+var app = {}
+app.init = function(){
+
+}
+app.send = function(message, data){
+  // args should be an object with
+  // keys:
+  //      type
+  //      data
+  //      url
+  //      callback
+  // if (!args['type'] || !args['data'] || !args['url']){
+  //   return Error('oops missing some args "type", "data", "url"')
+  // }
+
+  $.ajax({ 
+        type: "POST",
+        url: "https://api.parse.com/1/classes/chatterbox",
+        data: JSON.stringify(message),
+        success: function(data){
+          console.log('chatterbox: message sent')
+        },
+        error: function(data){
+          console.log('oops: chatterbox boxchattered')
+        }
+      })
+}
+
+
+app.fetch = function(){
+  
+}
+
+
+
+
+
+// jQuery.post( url [, data ] [, success ] [, dataType ] )
+// shorthand for
+// $.ajax({
+//   type: "POST",
+//   url: url,
+//   data: data,
+//   success: success,
+//   dataType: dataType
+// });
