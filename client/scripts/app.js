1e1076cf189bdd1b2f2a222dcac0427add7f35b1
Student <student@MKS-SF-4.local>
2015-05-04 18:49:00 -0500

    addFriend bolding added

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,18 @@
 // YOUR CODE HERE:
 
+
+
 var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox'
+  server: 'https://api.parse.com/1/classes/chatterbox',
 }
 app.init = function(){
+
+$(document).ready(function(){
+  $('body').on('click','.username', function(){
+    console.log('clicked the '+ this)
+    app.addFriend($(this))
+  });
+});
 
 }
 app.send = function(message, data){
@@ -53,13 +62,32 @@ app.clearMessages = function(){
 
 app.addMessage = function(message){
   // look into how to properly append
-  $('#chats').append('<div class="message">'+ JSON.stringify(message) +'</div>')
+  var $chats = $('#chats')
+  var $messageContainer = $('<div class="message"></div>')
+  $messageContainer.attr('username',message.username)
+  var $username = $('<div class="username"></div>')
+  var $text = $('<div class="text"></div>')
+  var $roomname = $('<div class="roomname"></div>') // FIXME: // should be .room add an attribute such as roomname="namehere" ??
+
+  $chats.append($messageContainer
+    .append(
+      $username.text(message.username), 
+      $text.text(message.text), 
+      $roomname.text(message.roomname)))
+
 }
 
 app.addRoom = function(roomName){
+    // FIXME: add id roomname
   $('#roomSelect').append('<div class="rooms">' + JSON.stringify(roomName) + '</div>')
 }
 
+app.addFriend = function($userNameContext){
+  var boldUser = $userNameContext.parent('div[username]').attr('username')
+  $("div.message:contains('" + boldUser + "')").css('font-weight','bold')
+}
+
+app.init()
 
 
 // jQuery.post( url [, data ] [, success ] [, dataType ] )
